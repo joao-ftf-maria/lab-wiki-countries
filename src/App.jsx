f02b466328e78b5f1f274f8d5c894882fe55c806
João Maria <joao.ftf.maria@gmail.com>
2020-08-17 11:59:58 +0100

    done with bonus

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './components/NavBar';
+import CountriesList from './components/CountriesList';
+
+import { Switch, Route } from 'react-router-dom';
+import CountryDetail from './components/CountryDetail';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div className="container d-flex p-2">
+        <CountriesList />
+        <Switch>
+          <Route exact path="/">
+            <h1>Please select a country</h1>
+          </Route>
+          <Route path="/:id" render={(props) => <CountryDetail {...props} />} />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CountryDetail.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class CountryDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      country: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        'https://restcountries.eu/rest/v2/alpha/' + this.props.match.params.id
+      )
+      .then((response) => {
+        this.setState({ country: response.data });
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      axios
+        .get(
+          'https://restcountries.eu/rest/v2/alpha/' + this.props.match.params.id
+        )
+        .then((response) => {
+          this.setState({ country: response.data });
+        });
+    }
+  }
+
+  render() {
+    const countryData = this.state.country;
+    let countryBorders = '';
+    if (countryData.borders) {
+      countryBorders = countryData.borders.map((border) => (
+        <li key={border}>
+          <Link to={`/${border}`}>{border}</Link>
+        </li>
+      ));
+    }
+    return (
+      <div className="p-2">
+        <h1>{countryData.name}</h1>
+        <table className="table">
+          <tbody>
+            <tr>
+              <th style={{ width: '400px' }}>Capital</th>
+              <td>{countryData.capital}</td>
+            </tr>
+            <tr>
+              <th>Area</th>
+              <td>{countryData.area}</td>
+            </tr>
+            <tr>
+              <th>Borders</th>
+              <td>
+                <ul>{countryBorders}</ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default CountryDetail;
